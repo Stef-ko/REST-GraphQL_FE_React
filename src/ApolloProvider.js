991ef736c149ec016f1b7021406200b08997ca21
SKZ <stefan.kraenzle@opitz-consulting.com>
2021-06-30 14:09:36 +0200

    Changed GraphQL URL

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -1,25 +1,26 @@
-import React from "react";
-import App from "./App";
+import React from 'react'
+import App from './App'
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from "@apollo/client";
+} from '@apollo/client'
 
-export const apolloURI = "http://localhost:5000/";
+// export const apolloURI = "http://localhost:5000/";
+export const apolloURI = 'https://thawing-earth-03793.herokuapp.com/'
 
 const httpLink = createHttpLink({
   uri: apolloURI,
-});
+})
 
 const client = new ApolloClient({
   link: httpLink,
   cache: new InMemoryCache(),
-});
+})
 
 export default (
   <ApolloProvider client={client}>
     <App />
   </ApolloProvider>
-);
+)
