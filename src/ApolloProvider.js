4619738bac5d50e0fbc9aef7c1e4e2d8b921d679
Kränzle <stefan.kraenzle@opitz-consulting.com>
2023-05-07 17:46:27 +0200

    Added _headers file to allow http origin

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -8,8 +8,7 @@ import {
 } from '@apollo/client'
 
 //export const apolloURI = 'http://localhost:5000/'
-// export const apolloURI = 'http://44.201.215.137:5000/'
-export const apolloURI = 'https://eager-northcutt-ae83d1.netlify.app/api'
+export const apolloURI = 'http://44.201.215.137:5000/'
 
 const httpLink = createHttpLink({
   uri: apolloURI,
